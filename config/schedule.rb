2cc766e95be347c68f0bc1a818e565f6f40a8536
Isaac Massot <isaac.mg@coditramuntana.com>
2019-02-01 10:50:46 +0100

    add env path to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,4 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+env :PATH, ENV['PATH']
 
 every :sunday, at: '4:00 am' do
   rake "decidim:delete_data_portability_files"
