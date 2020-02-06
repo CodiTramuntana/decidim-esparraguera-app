695aa2ae9635e031f29b86f0db189b5475f7ee24
Víctor Ortega <victor.ol@coditramuntana.com>
2020-02-06 09:26:21 +0100

    Removed forcing time-zone in DateTime inputs

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module DevelopmentApp
     
     config.time_zone = 'Madrid'
     config.active_record.default_timezone = :local
-    config.active_record.time_zone_aware_attributes = false
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
